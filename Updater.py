68753ec809813f3b1b9465585a871c2342daf124
GermanBot <85112385+LeakerByDragon3@users.noreply.github.com>
2021-06-01 16:46:27 +0200

    Update Updater.py

--- Updater.py
+++ Updater.py
@@ -2,7 +2,7 @@ import time,requests,os,traceback,sys,json
 
 with open("fortnite.py", encoding='utf-8') as f:
     current = f.read().replace('“','"').replace('”','"')
-github = requests.get("https://raw.githubusercontent.com/LeakerByDragon3/GermanBot-V-0.1/main/fortnite.py").text.replace('“','"').replace('”','"')
+github = requests.get("https://raw.githubusercontent.com/LeakerByDragon3/GermanBot-V-0.1/main/fortnite_update.py").text.replace('“','"').replace('”','"')
 if current != github:
     print('Update Gefunden! Bitte warten...')
     os.remove("fortnite.py")
